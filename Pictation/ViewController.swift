b69f7bb3fc6eb6c145b4182eac27f0f88deb20e5
Benton <benton@d192-075-243-126.wireless.sfu.ca>
2017-10-25 11:57:38 -0700

    Initial Commit

---
+++ Pictation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pictation
+//
+//  Created by Benton on 2017-10-25.
+//  Copyright © 2017 Benton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
