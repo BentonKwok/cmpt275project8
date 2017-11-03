3abe4fcf6cd3ff263d3aa4ac6d758ad1b551201f
Jameson Roy <jameson@Jamesons-iMac.local>
2017-11-03 16:32:15 -0700

    Working Intermediate View

---
+++ Pictation/ImageDisplay.swift
@@ -0,0 +1,74 @@
+//
+//  ImageDispaly.swift
+//  Pictation
+//
+//  Created by Jameson Roy on 10/29/17.
+//  Copyright © 2017 Benton. All rights reserved.
+//
+
+import UIKit
+
+/* A UIStackView subclass to implement the displayed images at the top of
+ * intermediate and advanced levels. It also keeps track of the words associated with the
+ * selected images and stores them in the selectedText property which is a String array
+ * that is not set to private in order to be accessible for sentence processing
+ */
+@IBDesignable class ImageDisplay: UIStackView {
+    //MARK: Properties
+    private var selectedImages = [UIImageView]()
+    private var numSubViews = 0
+    @IBInspectable var imageSize: CGSize = CGSize(width: 60.0, height: 60.0)
+    var selectedText = [String]()
+    
+    
+    //MARK: Init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addArrangedSubview(UIImageView())
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        addArrangedSubview(UIImageView())
+    }
+    
+    //MARK: Actions
+    //Adds images and text passed in into the their containers in the object
+    func addImage( newImage : UIImage?, newText : String?, maxSubViews : Int){
+        if(numSubViews < maxSubViews){
+            let newImageView = UIImageView()
+            guard (newText != nil && newImage != nil) else{
+                print("Passed in Image or Txt is nil in: ImageDisplay.addImage( )")
+                return
+            }
+            newImageView.image = newImage!
+            //a bunch of properites to make sure the image views resize correctly
+            newImageView.autoresizingMask = UIViewAutoresizing(rawValue: UIViewAutoresizing.RawValue(UInt8(UIViewAutoresizing.flexibleBottomMargin.rawValue) | UInt8(UIViewAutoresizing.flexibleHeight.rawValue) | UInt8(UIViewAutoresizing.flexibleRightMargin.rawValue) | UInt8(UIViewAutoresizing.flexibleLeftMargin.rawValue) | UInt8(UIViewAutoresizing.flexibleTopMargin.rawValue) | UInt8(UIViewAutoresizing.flexibleWidth.rawValue)))
+            newImageView.contentMode = UIViewContentMode.scaleAspectFit
+            newImageView.translatesAutoresizingMaskIntoConstraints = false
+            newImageView.heightAnchor.constraint(equalToConstant: imageSize.height).isActive = true
+            newImageView.widthAnchor.constraint(equalToConstant: imageSize.width).isActive = true
+          
+            selectedImages.append(newImageView)
+            selectedText.append(newText!)
+            
+            //add to the stack
+            addArrangedSubview(newImageView)
+            numSubViews += 1
+        }
+    }
+    
+    //resets the ImageDisplayObject
+    func reset( ){
+        if(numSubViews > 0){
+            selectedText.removeAll()
+            //remove pictures from the stack
+            for view in selectedImages{
+                view.removeFromSuperview()
+            }
+            
+            selectedImages.removeAll()
+            numSubViews = 0
+        }
+    }
+}
---
+++ Pictation/IntermediateViewController.swift
@@ -0,0 +1,100 @@
+  //
+//  IntermediateViewController.swift
+//  Pictation
+//
+//  Created by Jameson Roy on 2017-11-01.
+//  Copyright © 2017 Benton. All rights reserved.
+//
+
+import UIKit
+
+class IntermediateViewController: UIViewController {
+    //MARK: Properties
+    @IBOutlet weak var collectionView: UICollectionView!
+    var currenctSelectedWord = ""
+    @IBOutlet weak var outputSentenceText: UITextField!
+    var selectedImage: UIImage!
+    @IBOutlet weak var sentenceImages: ImageDisplay!
+    
+   // @IBAction func makeButtonHandler(_ sender: UIButton) {
+    @IBAction func makeButtonHandler(_ sender: UIButton) {
+        //A check to make sure selectedText is not accessed when it doesn't have elements yet
+        if (currenctSelectedWord != "") {
+            var sentence : String = ""
+            for i in 0...(self.sentenceImages.selectedText.count-1){
+                sentence += self.sentenceImages.selectedText[i]
+                sentence += " "
+            }
+                outputSentenceText.text = sentence
+        }
+    }
+
+    @IBAction func restartButtonHandler(_ sender: UIButton
+        ) {
+        currenctSelectedWord = ""
+        selectedImage = nil
+        outputSentenceText.text = currenctSelectedWord
+        self.sentenceImages.reset()
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
+
+/// Collection View Impl
+//extension BeginnerLevelViewController : UICollectionViewDelegate {
+//    func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+//        print("styuen: should select item at location \(indexPath.item)")
+//        return true
+//    }
+//    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+//        print("styuen: item: \(indexPath.item) was selected")
+//    }
+//}
+
+/// Collection View data
+extension IntermediateViewController : UICollectionViewDataSource {
+    /// Number of section and items in each section
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return Asset.allImages.count
+    }
+    
+    /// Create cell for each item
+    // In buttonHandler, update currentSelectedWord and the selectedImage when "Make" button is clicked
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! myCell
+        cell.buttonCell.setBackgroundImage(Asset.allImages[indexPath.row].image, for: .normal)
+        cell.buttonHandler = { [weak self] button in
+            self?.currenctSelectedWord = Asset.allImages[indexPath.row].name
+            self?.selectedImage = Asset.allImages[indexPath.row].image
+            
+            //adds images and text to subview at the top
+            self?.sentenceImages.addImage(newImage: (self?.selectedImage), newText : (self?.currenctSelectedWord), maxSubViews: 3)
+
+            
+            //print("bentonk: buttonHandler on item: \(indexPath.item) selected")
+        }
+        //cell.buttonCell.setAttributedTitle(NSAttributedString.init(string: "test"), for: .normal)
+        
+        return cell
+    }
+}
+
