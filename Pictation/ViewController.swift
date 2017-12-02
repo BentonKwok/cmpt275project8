540386b8c4dbecaebb19cf3a3f7de7a9c85dca76
aahuja <aahuja@sc-3100-11.labs.surrey.sfu.ca>
2017-12-01 21:26:50 -0800

    Ashmit Ahuja HelperView

--- Pictation/ViewController.swift
+++ Pictation/ViewController.swift
@@ -1,9 +1,37 @@
-import UIKit
+//
+//  ViewController.swift
+//  HelpViewController
+//
+//  Created by Ashmit Ahuja on 2017-11-26.
+//  Copyright © 2017 Ashmit Ahuja. All rights reserved.
+//
 
-class ViewController: UIViewController {
+import UIKit
+import Foundation
+import MessageUI
+
+class ViewController: UIViewController, MFMailComposeViewControllerDelegate {
     
-    @IBAction func GuestButtonHandler(_ sender: UIButton) {
-        performSegue(withIdentifier: "toLevelViewSegue", sender: self)
+    
+    @IBAction func DiscussionLink(_ sender: UIButton) {
+        
+        if let url = NSURL(string:"https://www.google.com"){
+            UIApplication.shared.open(url as URL)
+        }
+    }
+    
+    @IBAction func SendAction(_ sender: UIButton) {
+        let mailComposeViewController = configuredMailComposeViewController()
+        if MFMailComposeViewController.canSendMail() {
+            
+            self.present(mailComposeViewController, animated: true, completion: nil)
+            
+            
+        }else{
+            self.showSendMailErrorAlert()
+            
+        }
+        
     }
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -14,5 +42,38 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func configuredMailComposeViewController() -> MFMailComposeViewController {
+        let mailComposerVC = MFMailComposeViewController()
+        mailComposerVC.mailComposeDelegate = self
+        mailComposerVC.setToRecipients(["edleung@sfu.ca"])
+        
+        mailComposerVC.setSubject("Pictation - User Messages")
+        mailComposerVC.setMessageBody("This is the message body", isHTML: false)
+        
+        return mailComposerVC
+        
+        
+        
+    }
+    
+    
+    func showSendMailErrorAlert() {
+        
+        let sendMailErrorAlert = UIAlertController(title: "Could not send email", message: "Your device must have an active mail account", preferredStyle: .alert)
+        
+        let cancelAction = UIAlertAction(title: "OK", style: .cancel)
+        
+        sendMailErrorAlert.addAction(cancelAction)
+        
+    }
+    
+    
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+
+
+
 }
 
